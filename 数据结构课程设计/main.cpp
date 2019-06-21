c00f97d58ee28dbb50bae699757b4acbf8591515
winnerwinter1998 <winnerwinter@126.com>
2019-06-21 17:40:34 +0800

    重构测试

--- 数据结构课程设计/main.cpp
+++ 数据结构课程设计/main.cpp
@@ -1,17 +1,35 @@
 ﻿#include "common.h"
 using namespace winnerwinter;
 
+void test();
+bool cycle(AdjMatrix adjMatrix);
+
 int main()
+{
+	test();
+	return 0;
+}
+
+
+void test() 
 {
 	AdjMatrix adjMatrix;
 	adjMatrix.createMatrix();
-	while (true) {
-		adjMatrix.printAdj(); 
-		adjMatrix.shortestPath();
-		adjMatrix.relativeConnectable();
-		adjMatrix.onceConnectable();
-		adjMatrix.updateXY();
-	}
-	system("pause");
-	return 0;
+LOOP:
+	if (cycle(adjMatrix)) goto LOOP;
+	else return;
+}
+
+bool cycle(AdjMatrix adjMatrix) 
+{
+	adjMatrix.printAdj();
+	adjMatrix.shortestPath();
+	adjMatrix.relativeConnectable();
+	adjMatrix.onceConnectable();
+	adjMatrix.updateXY();
+	std::cout << "输入\"exit\"退出，输入\"continue\"或其他内容继续" << std::endl;
+	std::string str;
+	std::cin >> str;
+	if (str == "exit" || str == "EXIT") return false;
+	else return true;
 }
